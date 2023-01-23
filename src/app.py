c6f938a51c231dea66c0934b46523f2b9ee5cdc0
faldea1 <faldea1@uc.cl>
2023-01-23 05:00:19 +0000

    API with endpoints GET's, POST, DELETE & PUT

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person.
+#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,17 +26,56 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id=None)
+    member = jackson_family.get_member(member_id)
+
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"message": "ID was not found"}), 400
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    data = request.get_json()
+
+    if isinstance(data, dict):
+        jackson_family.add_member(data)
+        return jsonify({"message": "new member incorporated"}), 200
+    else:
+        return jsonify({"meesage": "the request has an error"}), 400
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id=None):
+    member = jackson_family.delete_member(member_id)
+
+    if member: 
+        return jsonify(member), 200
+    else:
+        return jsonify({"message": "ID was not found"}), 400
+
+
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def refresh_member(member_id=None):
+    data = request.get_json()
+    member = jackson_family.update_member(member_id, data)
+
+    if member:
+        return jsonify({"message":"member has been updated"}), 200
+    else:
+        return jsonify({"message":"ID was not found"}), 400
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
